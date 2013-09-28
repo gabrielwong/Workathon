575f4cfcecd5f8c09c728b9dbcd84ee86c11de45
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-09-28 05:10:59 -0400

    popup design v1 completed

--- popup.js
+++ popup.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('a').click(function(){
        chrome.tabs.create({url: $(this).attr('href')});
     });
+    
+    $("a").mouseenter(function() {
+        $(this).css({"padding-top": 2});
+    });
+    
+    $("a").mouseleave(function() {
+        $(this).css({"padding-top": 0});
+    });
 });
