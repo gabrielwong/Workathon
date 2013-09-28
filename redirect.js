7133500a58dce03dc5cc4cce209275ce1459149d
Gabriel Wong <contact@gabrielwong.net>
2013-09-28 08:40:14 -0400

    Added functionality to disable filter and update filter from chrome sync

--- redirect.js
+++ redirect.js
@@ -1,9 +1,16 @@
-function getRedirectUrl() {
-    return "http://google.ca";
+var filterActive = true;
+
+function setFilterActive(active){
+    filterActive = active;
+}
+
+function getRedirectUrl(oldUrl) {
+    return "https://www.google.ca/#q=" + oldUrl;
 }
 
 function redirectRequest(details){
-    return {redirectUrl: getRedirectUrl()};
+    if (filterActive)
+        return {redirectUrl: getRedirectUrl(details.url)};
 }
 
 function Filter(urls){
@@ -11,16 +18,22 @@ function Filter(urls){
     this.types = ["main_frame"];
 }
 
+// Update the redirect with new urls
 function bindFilter(urls){
     if (chrome.webRequest.onBeforeRequest.hasListener(redirectRequest)){
         chrome.webRequest.onBeforeRequest.removeListener(redirectRequest);
     }
     var filter = new Filter(urls);
-    chrome.webRequest.onBeforeRequest.addListener(redirectRequest,
-                                                  filter,
-                                                  ["blocking"]);
+    chrome.webRequest.onBeforeRequest.addListener(  redirectRequest,    
+                                                    filter,
+                                                    ["blocking"]);
+}
+
+// Updates the redirect with URLs stored in chrome storage using key
+function updateFilter(key){
+    chrome.storage.sync.get(key, function(items){
+        bindFilter(items[key]);
+    });
 }
 
 bindFilter(["http://reddit.com/*"]);
-
-alert("success");
