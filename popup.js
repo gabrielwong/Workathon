f904bb60fe3460ceb2c2fc850eafd31fead56b5e
Hamdan Javeed <hamdan@hamdanjaveed.com>
2013-09-28 13:11:49 -0400

    fixed double tab bug

--- popup.js
+++ popup.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-    $('a').click(function(){
-        chrome.tabs.create({url: $(this).attr('href')});
-        return;
-    });
-    
     $("a").mouseenter(function() {
         $(this).css({"padding-top": 2});
     });
