27b82f1a00e5f51b7afa950b343a601415e0251b
Gabriel Wong <contact@gabrielwong.net>
2013-10-04 21:41:11 -0400

    Fix block site in popup

--- js/popup.js
+++ js/popup.js
@@ -1,38 +1,10 @@
 $(document).ready(function () {
     $("#block").click(function () {
-        chrome.tabs.query({active:true,currentWindow:true},function(tab){ 
-            addInputSite(tab[0].url);
+        chrome.tabs.query({active:true,currentWindow:true},function(tab){
+            chrome.runtime.getBackgroundPage(function(page){
+                var site = page.parseURLToFilter(tab[0].url);
+                page.addBlockedSite(site);
+            });
         });
     });
 });
-
-function parseURLToFilter(url){
-    var a = $('<a>', { href:url } )[0];
-
-    return "*://*.".concat(a.hostname, "/*");
-}
-
-function addInputSite(url){
-    var site = url;
-    if(!site) {
-        return;
-    }
-    
-    chrome.storage.sync.get("BLACKLIST", function (items) {
-        var blacklist = items["BLACKLIST"];
-        if (blacklist == null){
-            blacklist = [];
-        }
-        blacklist.push(parseURLToFilter(site));
-        blacklist.sort();
-        chrome.storage.sync.set({"BLACKLIST": blacklist}, function () {
-            updateFilter("BLACKLIST");
-        });
-    });
-}
-
-function updateFilter(urls){
-    chrome.runtime.getBackgroundPage(function(page) {
-        page.updateFilters("BLACKLIST");
-    });
-}
